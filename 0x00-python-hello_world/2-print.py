445ec81e1157f13a5506562303321efbcb7aadde
itsmraga-hub <itsragamit@gmail.com>
2022-08-22 06:01:11 -0700

    Third commit

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("\"Progrmming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{number} Battery sweet")
+print("{:d} Battery sweet".format(number))
