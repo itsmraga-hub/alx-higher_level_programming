7f789da53a1935604009c5d8fc1d25f0a0ad3b3d
itsmraga-hub <itsragamit@gmail.com>
2022-08-23 09:36:38 -0700

    FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+	for i in range(0, 100):
+		if i % 3 == 0 and i % 5 == 0:
+			print("FizzBuzz", end='')
+		elif i % 3 == 0:
+			print("Fizz", end='')
+		elif i % 5 == 0:
+			print("Buzz", end='')
+		else:
+			print(i, end='')
+
+		print(" ", end='')
+			
