92b0ffcbd5ba0bcca868ed30a4a46f5a2b434c09
itsmraga-hub <itsragamit@gmail.com>
2022-08-25 02:34:40 -0700

    Second commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
-print(f"{a} + {b} = {add_0.add(a, b)}")
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
