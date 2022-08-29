b9c680c195e48a3f823bb1620caa15c322352098
itsmraga-hub <itsragamit@gmail.com>
2022-08-29 07:44:22 -0700

    Second commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for num in my_list:
-        print(num)
+        print("{}".format(num))
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-replace_in_list = __import__('2-replace_in_list').replace_in_list
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+print_reversed_list_integer(my_list)
