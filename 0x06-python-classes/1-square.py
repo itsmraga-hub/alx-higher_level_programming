02fd246505bbc5e834aba925dfd291a847bb859b
itsmraga-hub <itsragamit@gmail.com>
2022-09-14 09:16:02 -0700

    Worked on square challenges

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """class Square"""
+
+    def __init__(self, size):
+        """Initializer
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
