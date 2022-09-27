aa5aa70403d120bddec1f1acdf2f1c3b40a07468
itsmraga-hub <itsragamit@gmail.com>
2022-09-27 10:45:53 -0700

    Work onf irst challenge

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that containing a function reading from a file """
+
+
+def read_file(filename=""):
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
