ac69f817d3aae829460e9a6b82f77201fdd792af
itsmraga-hub <itsragamit@gmail.com>
2022-09-27 10:46:46 -0700

    Work on jso projects

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that containing a function returning the JSON
+representation of an object
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object
+    Args:
+        my_obj: object
+    Raises:
+        Exception: when the object can't be encoded
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that containing a function returning an object by
+a JSON representation
+"""
+import json
+
+
+def from_json_string(my_str):
+    """ Function that returns an object by a JSON representation
+    Args:
+        my_str: JSON representation
+    Raises:
+        Exception: when the string can't be decoded
+    """
+    return json.loads(my_str)
