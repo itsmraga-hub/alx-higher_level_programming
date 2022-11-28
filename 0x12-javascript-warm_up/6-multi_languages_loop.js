be090b0b441f59f1422fe4b7248bbca65830b834
itsmraga-hub <itsragamit@gmail.com>
2022-11-28 11:55:16 +0300

    Work on challenge 7

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const list = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < list.length; i++) {
+	console.log(list[i]);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+if (process.argv.length < 3) {
+	console.log("Missing number of occurrences");
+} else {
+	const num = parseInt(process.argv[2]);
+	if (!isNaN(num)) {
+		for (let i = 0; i < num; i++) {
+			console.log("C is fun");
+		}
+	} else {
+		console.log("Missing number of occurrences");
+	}
+}
