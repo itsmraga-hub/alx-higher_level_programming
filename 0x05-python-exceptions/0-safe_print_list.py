2bac4800eb15a6135dab2f9b9dfc416f63e2d718
root <root@Raga>
2023-01-23 13:57:51 +0300

    Add exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
+        except IndexError:
             break
         else:
             num += 1
