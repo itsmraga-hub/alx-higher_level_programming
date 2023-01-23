acf2ebe5fd6f4cb742366dc7a30569a592426d67
itsmraga-hub <ragawilliam570@gmail.com>
2023-01-23 14:27:03 +0300

    Work on tassk 4

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         res = a / b
-    except:
+    except ZeroDivisionError:
         res = None
     finally:
         print("Inside result: {}".format(res))
