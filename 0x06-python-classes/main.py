95318534726f7af2a436ad044b324e0f3f3330ef
itsmraga-hub <ragawilliam570@gmail.com>
2023-01-24 20:16:40 +0300

    Add advanced solution

--- 0x06-python-classes/main.py
+++ 0x06-python-classes/main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
+Square = __import__('6-square').Square
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
+my_square_1 = Square(3)
+my_square_1.my_print()
 
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
+print("--")
 
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
