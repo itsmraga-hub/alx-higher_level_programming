997454022568af1e6e2732bbe3dd30182366ae5a
itsmraga-hub <ragawilliam570@gmail.com>
2023-01-24 20:08:14 +0300

    Fix docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 class Square:
-    """class defining a Square
+    """A class defining an empty Square.
+
+        If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
     """
     pass
