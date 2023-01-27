cb21ab603782f55bdc6a7f416630cae27bd5fa11
itsmraga-hub <ragawilliam570@gmail.com>
2023-01-27 21:16:15 +0300

    Add 2-matrix comment

--- 0x07-python-test_driven_development/2-matrix-divided.py
+++ 0x07-python-test_driven_development/2-matrix-divided.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This module is of a function that divides the numbers of a matrix
+This module is of a function that divides the numbers of a
+matrix and returns the new matrix
 """
 
 
