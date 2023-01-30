e43e23ce9906372475f825488e121930c46d9e24
itsmraga-hub <ragawilliam570@gmail.com>
2023-01-30 18:56:29 +0300

    Add full soln for 8-rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -7,7 +7,9 @@ This module is of a class defining a Rectangle
 class Rectangle:
     """ Class defining a rectangle """
 
-    
+    number_of_instances = 0
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         """ Method initializng an instance
         Args:
@@ -16,6 +18,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
