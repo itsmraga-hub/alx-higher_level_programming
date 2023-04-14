c84f219a28129ebe8bb5c33285e7ab048b7315c8
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-14 10:18:22 +0300

    Task 4 - Join citites and states table query

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+"""
+    A script that lists all states with a name starting with N
+    from the database
+"""
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+    cursor = conn.cursor()
+    sql = """SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"""
+    cursor.execute(sql)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    conn.close()
