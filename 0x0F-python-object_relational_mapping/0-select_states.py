4d4dcea6eeccf1c7393263e518883e6a985c5dba
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-16 23:54:22 +0300

    Fix errrs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
-import MySQLdb
-import sys
-
 """
     A python script that lists all states from the database
 """
 
+import MySQLdb
+import sys
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-               host="localhost",
                user=sys.argv[1],
                password=sys.argv[2],
                database=sys.argv[3],
-               port=3306
             )
     cursor = conn.cursor()
-    sql = """SELECT * FROM states ORDER BY id ASC"""
+    sql = """SELECT * FROM `states` ORDER BY id ASC"""
     cursor.execute(sql)
     states = cursor.fetchall()
     for state in states:
         print(state)
-
-    cursor.close()
-    conn.close()
