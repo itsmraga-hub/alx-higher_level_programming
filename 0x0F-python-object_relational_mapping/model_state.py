16f549f53464a12a4be45486a65cc354081cfdc7
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-18 01:37:37 +0300

    Update relationship btn city and state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
@@ -20,5 +20,6 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", back_populates="state")
