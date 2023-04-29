66d129474567ad7e173eea9cc4a8f8b84f741645
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-29 23:50:48 +0300

    Task 6: takes URL and POSTs email using requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    A python script that takes in a URL and an email address, sends a POST
+    request to the passed URL with email as a parameter
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    res = requests.post(url, params=payload)
+    print(res.text)
