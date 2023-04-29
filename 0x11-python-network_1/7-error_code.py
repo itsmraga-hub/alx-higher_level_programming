e34539f5bf025320803d9e38e13e1ccf8bace3eb
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-29 23:50:12 +0300

    Task 7: takes URL, send request using requests and displays body of response

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    A python script that takes in a URL, sends a request to the URL and
+    displays the body of the response
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("Error code: ", res.status_code)
