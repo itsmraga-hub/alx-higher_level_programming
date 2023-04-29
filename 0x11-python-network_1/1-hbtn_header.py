0a6d4c17303d194e422b6d242f44f3895d9c36bb
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-29 23:47:03 +0300

    Task 1: complete

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     with urllib.request.urlopen(url) as response:
-        x_req_id = response.headers['X-Request-Id']
+        x_req_id = response.headers.get('X-Request-Id')
         print(x_req_id)
