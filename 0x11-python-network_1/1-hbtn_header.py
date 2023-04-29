ddb2f76669c39c4f232abbe454c5fc8be693d8e0
itsmraga-hub <ragawilliam570@gmail.com>
2023-04-29 12:33:50 +0300

    Task 1: fetch url passed as argument

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    A python script that takes in a URL, sends a request and displays the
+    value of the X-Request-Id variable found in the header of the response
+"""
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        x_req_id = response.headers['X-Request-Id']
+        print(x_req_id)
