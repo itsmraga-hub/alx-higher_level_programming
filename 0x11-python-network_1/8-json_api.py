8a5593e8fa1efa0e060e738686e89fcc8ac2b2d5
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-01 12:31:33 +0300

    Task 8 correction

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print("[{}] {}".format(res.get('id'), res.get('name')))
-    except:
+    except Exception:
         print("Not a valid JSON")
