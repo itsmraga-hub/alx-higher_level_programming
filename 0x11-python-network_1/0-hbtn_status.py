0f0fb72be3fea2f587d375ad06e69dc8bd9fecdb
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-01 11:22:36 +0300

    Task 0: correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         body = res.read()
         print("Body response:")
-        print("    - type: " + str(type(body)))
-        print("    - content: " + str(body))
-        print("    - utf8 content: " + str(body.decode('utf-8')))
+        print("\t- type: " + str(type(body)))
+        print("\t- content: " + str(body))
+        print("\t- utf8 content: " + str(body.decode('utf-8')))
