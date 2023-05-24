df1d71e773218648be84db9c78c0cd99c953297d
itsmraga-hub <ragawilliam570@gmail.com>
2023-05-24 03:49:56 +0300

    Task 6: fix semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 // A script that computes the number of tasks completed bu user id
 
-const fs = require('fs');
 const request = require('request');
 
 const url = process.argv[2];
-let all_tasks = [];
+let allTasks = [];
 const output = {};
 
 try {
@@ -13,16 +12,16 @@ try {
     if (err) {
       console.log('error:', err);
     } else {
-      all_tasks = JSON.parse(body);
+      allTasks = JSON.parse(body);
       for (let i = 0; i < 10; i++) {
-	let userId = i + 1;
-	let count = 0;
-	for (let j = 0; j < all_tasks.length; j++) {
-	  if (all_tasks[j].userId == userId && all_tasks[j].completed) {
-	    count++;
-	  }
-	}
-	output[userId] = count;
+        const userId = i + 1;
+        let count = 0;
+        for (let j = 0; j < allTasks.length; j++) {
+          if (allTasks[j].userId === userId && allTasks[j].completed) {
+            count++;
+          }
+        }
+        output[userId] = count;
       }
       console.log(output);
     }
