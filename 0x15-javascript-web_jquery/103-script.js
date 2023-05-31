9fdc139008089eaa1bf64bec68abd93aee3d7960
Raga <ragawilliam570@gmail.com>
2023-05-31 14:59:49 +0300

    Add translate function

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -10,14 +10,15 @@ $('document').ready(() => {
   $('INPUT#language_code').focus(() => {
     $(this).keydown((e) => {
       if (e.keyCode === 13) {
-        () => {
-          const url = 'https://www.fourtonfish.com/hellosalut/?';
-          $.get(url + $.param({ lang: $('INPUT#language_code').val() }),
-            (data) => {
-              $('DIV#hello').html(data.hello);
-            });
-        }
+        translate()
       }
     });
   });
 });
+
+const translate = () => {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+    $('DIV#hello').html(data.hello);
+  });
+}
