1257f1db77ea5e07b71852c751bf85ef8c539526
Raga <ragawilliam570@gmail.com>
2023-05-31 14:55:27 +0300

    Add function to translate on Enter key press

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -12,19 +12,12 @@ $('document').ready(() => {
       if (e.keyCode === 13) {
         () => {
           const url = 'https://www.fourtonfish.com/hellosalut/?';
-          $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-            $('DIV#hello').html(data.hello);
-          });
+          $.get(url + $.param({ lang: $('INPUT#language_code').val() }),
+            (data) => {
+              $('DIV#hello').html(data.hello);
+            });
         }
-      }
       }
     });
   });
 });
-
-// const translate = () {
-//   const url = 'https://www.fourtonfish.com/hellosalut/?';
-//   $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-//     $('DIV#hello').html(data.hello);
-//   });
-// }
